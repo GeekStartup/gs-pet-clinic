f47d419fc63043c6e23a2a9b144ac603d12ddfb1
Ashish Nayak <ashish04021989@gmail.com>
2020-06-04 17:37:54 -0500

    Initial commit

---
+++ src/main/java/geekstartup/springframework/gspetclinic/GsPetClinicApplication.java
@@ -0,0 +1,13 @@
+package geekstartup.springframework.gspetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/geekstartup/springframework/gspetclinic/GsPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package geekstartup.springframework.gspetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
