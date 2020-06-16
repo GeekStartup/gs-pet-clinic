a28c3a7e7c37c2990b7cab8a8a0b0670ca5ed9ac
Ashish Nayak <ashish04021989@gmail.com>
2020-06-15 22:49:34 -0500

    Changing Index page and adding Controller

---
+++ pet-clinic-web/src/main/java/geekstartup/springframework/gspetclinic/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package geekstartup.springframework.gspetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by ashishnayak on 15-Jun-2020||10:37 PM
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String Index() {
+        return "index";
+    }
+}
