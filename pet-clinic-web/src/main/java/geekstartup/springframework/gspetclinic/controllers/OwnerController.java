b2eb7c05c344e933071137a3f72213a7fc99c7d3
Ashish Nayak <ashish04021989@gmail.com>
2020-06-17 18:36:23 -0500

    Changing Owner Controller and template to display list of owners

--- pet-clinic-web/src/main/java/geekstartup/springframework/gspetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/geekstartup/springframework/gspetclinic/controllers/OwnerController.java
@@ -1,6 +1,8 @@
 package geekstartup.springframework.gspetclinic.controllers;
 
+import geekstartup.springframework.gspetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,8 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
