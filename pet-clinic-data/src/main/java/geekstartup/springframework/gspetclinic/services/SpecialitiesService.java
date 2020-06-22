97050aea7e8444f1da68e8badcb20d88476dee12
Ashish Nayak <ashish04021989@gmail.com>
2020-06-21 22:34:03 -0500

    Added SpecialityMapService impl

---
+++ pet-clinic-data/src/main/java/geekstartup/springframework/gspetclinic/services/SpecialitiesService.java
@@ -0,0 +1,9 @@
+package geekstartup.springframework.gspetclinic.services;
+
+import geekstartup.springframework.gspetclinic.model.Speciality;
+
+/**
+ * Created by ashishnayak on 21-Jun-2020||10:24 PM
+ */
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/geekstartup/springframework/gspetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,39 @@
+package geekstartup.springframework.gspetclinic.services.map;
+
+import geekstartup.springframework.gspetclinic.model.Speciality;
+import geekstartup.springframework.gspetclinic.services.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Created by ashishnayak on 21-Jun-2020||10:25 PM
+ */
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
