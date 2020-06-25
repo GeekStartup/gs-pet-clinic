23700299b1d3b33b1e503b51b63b890f2f93842c
Ashish Nayak <ashish04021989@gmail.com>
2020-06-24 22:40:46 -0500

    Updated BaseEntity.java with JPA super class

--- pet-clinic-data/src/main/java/geekstartup/springframework/gspetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/geekstartup/springframework/gspetclinic/model/BaseEntity.java
@@ -1,12 +1,19 @@
 package geekstartup.springframework.gspetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * Created by ashishnayak on 15-Jun-2020||6:14 PM
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
