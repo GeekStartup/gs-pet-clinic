47bbc7706ebc0f76d6f7460767632ab4e1b4c8d4
Ashish Nayak <ashish04021989@gmail.com>
2020-06-25 13:16:54 -0500

    Updated Vet and Speciality to Entities

--- pet-clinic-data/src/main/java/geekstartup/springframework/gspetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/geekstartup/springframework/gspetclinic/model/Speciality.java
@@ -1,10 +1,17 @@
 package geekstartup.springframework.gspetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by ashishnayak on 21-Jun-2020||7:19 PM
  */
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/geekstartup/springframework/gspetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/geekstartup/springframework/gspetclinic/model/Vet.java
@@ -1,13 +1,23 @@
 package geekstartup.springframework.gspetclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Created by ashishnayak on 10-Jun-2020||8:48 PM
  */
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
