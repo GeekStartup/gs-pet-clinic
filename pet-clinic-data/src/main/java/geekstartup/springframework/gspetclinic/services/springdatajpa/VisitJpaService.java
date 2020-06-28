ce3249ced21b25fe7daa7f417aaa84a1667d5747
Ashish Nayak <ashish04021989@gmail.com>
2020-06-27 22:48:32 -0500

    Added VisitJpaService

---
+++ pet-clinic-data/src/main/java/geekstartup/springframework/gspetclinic/services/springdatajpa/VisitJpaService.java
@@ -0,0 +1,51 @@
+package geekstartup.springframework.gspetclinic.services.springdatajpa;
+
+import geekstartup.springframework.gspetclinic.model.Visit;
+import geekstartup.springframework.gspetclinic.repositories.VisitRepository;
+import geekstartup.springframework.gspetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by ashishnayak on 27-Jun-2020||10:44 PM
+ */
+@Service
+@Profile("springdatajpa")
+public class VisitJpaService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
