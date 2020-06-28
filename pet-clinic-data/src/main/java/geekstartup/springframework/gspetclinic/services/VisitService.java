01a116aa376055b1bb3ad661e1cc82d4df402ea1
Ashish Nayak <ashish04021989@gmail.com>
2020-06-27 22:43:46 -0500

    Added VisitService and its Map impl

---
+++ pet-clinic-data/src/main/java/geekstartup/springframework/gspetclinic/services/VisitService.java
@@ -0,0 +1,9 @@
+package geekstartup.springframework.gspetclinic.services;
+
+import geekstartup.springframework.gspetclinic.model.Visit;
+
+/**
+ * Created by ashishnayak on 27-Jun-2020||10:33 PM
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/geekstartup/springframework/gspetclinic/services/map/VistMapService.java
@@ -0,0 +1,42 @@
+package geekstartup.springframework.gspetclinic.services.map;
+
+import geekstartup.springframework.gspetclinic.model.Visit;
+import geekstartup.springframework.gspetclinic.services.VisitService;
+
+import java.util.Set;
+
+/**
+ * Created by ashishnayak on 27-Jun-2020||10:34 PM
+ */
+public class VistMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+
+        if (visit.getPet() == null || visit.getPet().getOwner() == null
+                || visit.getPet().getOwner().getId() == null || visit.getPet().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
