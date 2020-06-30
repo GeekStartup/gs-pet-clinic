0b05d53566a4c0446130ab954bf33888acd03633
Ashish Nayak <ashish04021989@gmail.com>
2020-06-30 17:55:38 -0500

    testing Circle Ci badge for failure

--- pet-clinic-web/src/test/java/geekstartup/springframework/gspetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/geekstartup/springframework/gspetclinic/controllers/OwnerControllerTest.java
@@ -63,7 +63,7 @@ class OwnerControllerTest {
         mockMvc.perform(get("/owners"))
                 .andExpect(status().isOk())
                 .andExpect(model().attribute("owners", hasSize(2)))
-                .andExpect(view().name("owners/index"));
+                .andExpect(view().name("owners/inde"));
     }
 
     @Test
